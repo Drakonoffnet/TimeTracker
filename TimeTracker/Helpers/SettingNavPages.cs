679f7030a2be1adbe6092a9f69221695776f2eee
dmitriy.ivanov <dmitriy.ivanov@BIRD>
2017-10-23 21:19:36 +0300

    new controller

---
+++ TimeTracker/Helpers/SettingNavPages.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeTracker.Helpers
+{
+    public static class SettingNavPages
+    {
+        public static string Index => "Index";
+        public static string IndexNavClass(ViewContext viewContext) => BaseNavPages.PageNavClass(viewContext, Index);
+    }
+}
--- TimeTracker/Views/Manage/ManageNavPages.cs
+++ TimeTracker/Views/Manage/ManageNavPages.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using TimeTracker.Helpers;
 
 namespace TimeTracker.Views.Manage
 {
@@ -19,20 +20,14 @@ namespace TimeTracker.Views.Manage
 
         public static string TwoFactorAuthentication => "TwoFactorAuthentication";
 
-        public static string IndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, Index);
+        public static string IndexNavClass(ViewContext viewContext) => BaseNavPages.PageNavClass(viewContext, Index);
 
-        public static string ChangePasswordNavClass(ViewContext viewContext) => PageNavClass(viewContext, ChangePassword);
+        public static string ChangePasswordNavClass(ViewContext viewContext) => BaseNavPages.PageNavClass(viewContext, ChangePassword);
 
-        public static string ExternalLoginsNavClass(ViewContext viewContext) => PageNavClass(viewContext, ExternalLogins);
+        public static string ExternalLoginsNavClass(ViewContext viewContext) => BaseNavPages.PageNavClass(viewContext, ExternalLogins);
 
-        public static string TwoFactorAuthenticationNavClass(ViewContext viewContext) => PageNavClass(viewContext, TwoFactorAuthentication);
+        public static string TwoFactorAuthenticationNavClass(ViewContext viewContext) => BaseNavPages.PageNavClass(viewContext, TwoFactorAuthentication);
     
-        public static string PageNavClass(ViewContext viewContext, string page)
-        {
-            var activePage = viewContext.ViewData["ActivePage"] as string;
-            return string.Equals(activePage, page, StringComparison.OrdinalIgnoreCase) ? "active" : null;
-        }
 
-        public static void AddActivePage(this ViewDataDictionary viewData, string activePage) => viewData[ActivePageKey] = activePage;
     }
 }
