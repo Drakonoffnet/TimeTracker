5391eb5af44d3d7e3e9f5055c689038f1881d563
Dima Ivanov <frog98@gmail.com>
2017-10-28 10:38:06 +0300

    - Fix DAL, Fix settings index page, add view

--- TimeTracker/Models/SettingsViewModels/ProjectViewModel.cs
+++ TimeTracker/Models/SettingsViewModels/ProjectViewModel.cs
@@ -1,15 +1,16 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace TimeTracker.Models.DAL
+namespace TimeTracker.Models.SettingsViewModels
 {
-    public class Project
+    public class ProjectViewModel
     {
-        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public int CountMembers {get; set;}
     }
 }
